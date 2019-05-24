b6449de4e22449f6e85ed58e4db99feb52fdbfe4
Bruno <cardosobrunob@gmail.com>
2019-05-23 21:50:08 -0300

    Setup DRF

--- employeemanager/settings.py
+++ employeemanager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core',
 ]
 
@@ -159,4 +160,16 @@ LOGGING = {
             'handlers': ['console', 'file']
         }
     }
+}
+
+# DRF definition
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
