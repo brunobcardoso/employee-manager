83df8b12267c146d1b2fbdb24407d8feb1d3e413
Bruno <cardosobrunob@gmail.com>
2019-05-23 22:27:31 -0300

    Create EmployeeSerializer

---
+++ core/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from core.models import Employee, Department
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    department = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Department.objects.all()
+    )
+
+    class Meta:
+        model = Employee
+        fields = ('name', 'email', 'department')
