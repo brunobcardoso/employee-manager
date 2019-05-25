5a121c981a6155f2605e79f40ffcf577d2ae5abd
Bruno <cardosobrunob@gmail.com>
2019-05-25 01:09:52 -0300

    Create department view

--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 
-from core.models import Employee
-from core.serializers import EmployeeSerializer
+from core.models import Employee, Department
+from core.serializers import EmployeeSerializer, DepartmentSerializer
 
 
 class EmployeeList(generics.ListCreateAPIView):
@@ -14,4 +14,16 @@ class EmployeeList(generics.ListCreateAPIView):
 class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    permission_classes = (IsAdminUser,)
+
+
+class DepartmentList(generics.ListCreateAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = (IsAdminUser,)
+
+
+class DepartmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
     permission_classes = (IsAdminUser,)
