9d49b8b56a66e7b79f449bfee038f1df4a319cde
brunobcardoso <cardosobrunob@gmail.com>
2020-10-02 00:22:14 -0300

    Replace django url() to re_path() to solve deprecation warning

--- employeemanager/urls.py
+++ employeemanager/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -40,11 +40,11 @@ urlpatterns = [
     path('', include_docs_urls(title='Employee Manager API')),
     path('token-auth/', obtain_jwt_token),
     path('', include('core.urls')),
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
         name='schema-redoc'),
     path('api-auth/', include('rest_framework.urls')),
 ]
